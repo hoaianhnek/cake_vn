aaf053f6cf25b76a6e1ad4b4522d7adebc88a102
hoaianh <anhtth@hp-backup.com>
2025-10-25 13:38:38 +0700

    set up lang

---
+++ resources/lang/vi/messages.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'cat_bvfresh' => 'BV Fresh',
+    'cat_bvsport' => 'BV Sport',
+    'cat_bvyummy' => 'BV Yummy',
+    'cat_crispy' => 'Quẩy Giòn',
+];
--- resources/views/user/layouts/layout.blade.php
+++ resources/views/user/layouts/layout.blade.php
@@ -8,18 +8,21 @@
     <meta name="base-url" content="{{ asset('/') }}">
     <title>Banh Viet</title>
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @yield('css')
 </head>
 <body class="antialiased bg-white font-sans">
     @include('user.layouts.header')
 
-    @include('user.layouts.temp')
+    {{-- @include('user.layouts.temp') --}}
 
     @yield('content')
 
     @include('user.layouts.footer')
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     @yield('js-script')
 </body>
 </html>
